d7745210864c19666cde92ca8e13f1dcbe8bfbe8
aandreba <paltryorphan96@gmail.com>
2024-11-19 21:11:47 +0100

    vv

--- src/controllers/product.php
+++ src/controllers/product.php
@@ -1,3 +1,18 @@
 <?php
 
+require __DIR__ . '/../models/product.php';
+
+if (!isset($_GET['id'])) {
+    http_response_code(400);
+    exit;
+}
+
+$id = intval($_GET['id']);
+$product = Product::Fetch($id);
+
+if (!$product) {
+    http_response_code(404);
+    exit;
+}
+
 require __DIR__ . '/../views/product.php';
