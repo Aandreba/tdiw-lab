c173df2e3bb72d9419efcbb9e8d6e3e9d9b11ade
Aandreba <aandreba@gmail.com>
2024-11-21 11:02:52 +0100

    vv

--- src/api/signup.php
+++ src/api/signup.php
@@ -4,7 +4,8 @@ require_once __DIR__ . "/../models/user.php";
 
 try {
     session_start();
-    $user = User::SignUp($_POST["username"], $_POST["email"], $_POST["password"]);
+    $email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
+    $user = User::SignUp($_POST["username"], $email, $_POST["password"]);
     $_SESSION["user"] = $user->id;
     header("Location: /?at=categories");
 } catch (Exception $e) {
--- src/views/product.php
+++ src/views/product.php
@@ -1,3 +1,14 @@
-<?php
+<link rel="stylesheet" href="/assets/product.css">
 
-var_dump($product);
+<div class="product">
+    <h1 id="name"><?= $product->name ?></h1>
+    <span id="price"></span>
+    <p id="desc"><?= $product->description ?></p>
+    <img id="img" src="<?= $product->img ?>">
+    <!-- $product->category_id !-->
+</div>
+
+<script>
+    const priceSpan = document.querySelector(".product #price");
+    priceSpan.innerHTML = MONEY_FORMAT.format(<?= $product->price / 100 ?>);
+</script>
