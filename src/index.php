bcd4f9b26aaab0b7c4161fc66b04ed62c8375bb7
Aandreba <aandreba@gmail.com>
2024-11-26 12:04:03 +0100

    error handling

--- src/index.php
+++ src/index.php
@@ -1,26 +1,26 @@
 <?php
-require_once __DIR__ . '/i18n.php';
-$page = isset($_GET['at']) ? $_GET['at'] : null;
-session_start();
+try {
+    require_once __DIR__ . '/i18n.php';
+    $page = isset($_GET['at']) ? $_GET['at'] : null;
+    session_start();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="assets/main.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-    <script src="assets/i18n.js"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="assets/main.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+        <script src="assets/i18n.js"></script>
+    </head>
 
-<body>
-    <?php require __DIR__ . '/views/navbar.php'; ?>
-    <main>
-        <?php
-        try {
+    <body>
+        <?php require __DIR__ . '/views/navbar.php'; ?>
+        <main>
+            <?php
             switch ($page) {
                 case "categories":
                     require __DIR__ . '/controllers/categories.php';
@@ -39,13 +39,14 @@ session_start();
                     echo "Not found";
                     break;
             }
-        } catch (Exception $e) {
-            http_response_code(500);
-            echo $e->getMessage();
-        }
-        ?>
-    </main>
+            ?>
+        </main>
 
-</body>
+    </body>
 
-</html>
+    </html>
+<?php
+} catch (Exception $e) {
+    http_response_code(500);
+    echo $e->getMessage();
+}
