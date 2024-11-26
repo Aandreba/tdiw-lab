d5a032768d251b1313b80c4e9f1dc9a84eec943d
Aandreba <aandreba@gmail.com>
2024-11-26 13:09:22 +0100

    fix redirect

--- src/api/signup.php
+++ src/api/signup.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/../models/user.php";
 try {
     session_start();
     $_SESSION["user"] = User::SignUp($_POST["username"], $_POST["email"], $_POST["password"], $_POST["address"], $_POST["city"], $_POST["zip"]);
-    header("Location: /?at=categories");
+    header("Location: ../?at=categories");
 } catch (Exception $e) {
     http_response_code(400);
     echo $e->getMessage();
