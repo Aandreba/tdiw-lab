a34509a1f8e28a5f321ff8e8939f0a13aceb7fa6
Aandreba <aandreba@gmail.com>
2024-11-26 12:54:18 +0100

    vv

--- src/views/signup.php
+++ src/views/signup.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="assets/signup.css">
 
-<form action="/api/signup.php" method="post" class="signup">
+<form action="api/signup.php" method="post" class="signup">
     <input type="text" name="username" placeholder="Username" required>
     <input type="email" name="email" placeholder="Email" required>
     <input type="password" name="password" placeholder="Password" required>
