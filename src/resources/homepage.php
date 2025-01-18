1a9e98ea1930e8b5838be90d906106ccd4efa5cd
Xavi1670093 <xavigra2004@gmail.com>
2025-01-18 14:29:56 +0100

    ww

--- src/resources/homepage.php
+++ src/resources/homepage.php
@@ -11,7 +11,7 @@
     
     <div class="homepage-container">
         <div class="image-container">
-            <img src="https://files.oaiusercontent.com/file-TmNNNDQSgHmKEq3EprJph5?se=2025-01-17T19%3A46%3A42Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D299%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Dimage.png&sig=tM0uEQ0zSWaYFakY4ktrpLHua4Gec4Wzi/c%2BjMlinhk%3D" alt="Inicio" class="homepage-image">
+            <img src="assets/icons/portada.png" alt="Inicio" class="homepage-image">
             <div class="button-container">
                 <a href="/lab/?at=categories" class="btn">BUY NOW!</a>
             </div>
