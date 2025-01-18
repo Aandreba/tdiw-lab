9666515253c238c691e47c1923cacb2ccb5d6931
Xavi1670093 <xavigra2004@gmail.com>
2025-01-18 23:42:23 +0100

    ordercsssolucion

--- src/views/orders.php
+++ src/views/orders.php
@@ -1,15 +1,15 @@
 <link rel="stylesheet" href="assets/orders.css?v=1.0">
 
-<?php foreach ($orders as $key => $order): ?>
-    
-    <div>
-        <span><?= $order["creation_date"] ?></span>
-        <span><?= round($order["total"] / 100, 2) ?>€</span>
-        <ul>
-            <?php foreach ($order["items"] as $item): ?>
-                <li><?= $item["name"] ?> - <?= $item["quantity"] ?> x <?= round(intval($item["price"]) / 100, 2) ?>€</li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-<?php endforeach; ?>
-
+<div class="orders-container">
+    <?php foreach ($orders as $key => $order): ?>
+        <div class="order">
+            <span class="order-date"><?= $order["creation_date"] ?></span>
+            <span class="order-total"><?= round($order["total"] / 100, 2) ?>€</span>
+            <ul class="order-items">
+                <?php foreach ($order["items"] as $item): ?>
+                    <li><?= $item["name"] ?> - <?= $item["quantity"] ?> x <?= round(intval($item["price"]) / 100, 2) ?>€</li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endforeach; ?>
+</div>
